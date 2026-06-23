673c92c0830d4dbe7713ecd530995c893e7fdf90
Anton Yablokov <stsav012@gmail.com>
2026-06-23 10:20:30 +0300

    Reuse `QTextDocument` instance

--- src/pycatsearch_qt/html_style_delegate.py
+++ src/pycatsearch_qt/html_style_delegate.py
@@ -1,11 +1,18 @@
-from qtpy.QtCore import QModelIndex, QPersistentModelIndex, QRect, QSize
+from qtpy.QtCore import QModelIndex, QObject, QPersistentModelIndex, QRect, QSize
 from qtpy.QtGui import QAbstractTextDocumentLayout, QPainter, QPalette, QTextDocument
 from qtpy.QtWidgets import QApplication, QStyle, QStyleOptionViewItem, QStyledItemDelegate
+
+from .utils import the
 
 __all__ = ["HTMLDelegate"]
 
 
+
 class HTMLDelegate(QStyledItemDelegate):
+    def __init__(self, parent: QObject | None = None) -> None:
+        super().__init__(parent)
+        self._doc: QTextDocument = QTextDocument(self)
+
     def paint(
         self,
         painter: QPainter,
@@ -16,33 +23,31 @@ class HTMLDelegate(QStyledItemDelegate):
         style: QStyle | None = option.widget.style() if option.widget else QApplication.style()
         if style is None:
             raise RuntimeError("Failed to get style")
-        doc: QTextDocument = QTextDocument(self)
-        doc.setHtml(option.text)
-        option.text = ""
-        style.drawControl(QStyle.ControlElement.CE_ItemViewItem, option, painter)
-        ctx: QAbstractTextDocumentLayout.PaintContext = QAbstractTextDocumentLayout.PaintContext()
-        if option.state & QStyle.StateFlag.State_Selected:
-            ctx.palette.setBrush(QPalette.ColorRole.Text, option.palette.highlightedText())
-        text_rect: QRect = style.subElementRect(QStyle.SubElement.SE_ItemViewItemText, option)
-        painter.save()
-        if option.state & QStyle.StateFlag.State_Selected:
-            painter.fillRect(option.rect, option.palette.highlight())
-        painter.translate(text_rect.topLeft())
-        painter.setClipRect(option.rect.translated(-text_rect.topLeft()))
-        painter.translate(0, 0.5 * (option.rect.height() - doc.size().height()))
-        doc.documentLayout().draw(painter, ctx)
-        painter.restore()
-        doc.deleteLater()
+        with the(self._doc) as doc:
+            doc.clear()
+            doc.setHtml(option.text)
+            option.text = ""
+            style.drawControl(QStyle.ControlElement.CE_ItemViewItem, option, painter)
+            ctx: QAbstractTextDocumentLayout.PaintContext = QAbstractTextDocumentLayout.PaintContext()
+            text_rect: QRect = style.subElementRect(QStyle.SubElement.SE_ItemViewItemText, option)
+            painter.save()
+            if option.state & QStyle.StateFlag.State_Selected:
+                painter.fillRect(option.rect, option.palette.highlight())
+                ctx.palette.setBrush(QPalette.ColorRole.Text, option.palette.highlightedText())
+            painter.translate(text_rect.topLeft())
+            painter.setClipRect(option.rect.translated(-text_rect.topLeft()))
+            painter.translate(0, 0.5 * (option.rect.height() - doc.size().height()))
+            doc.documentLayout().draw(painter, ctx)
+            painter.restore()
 
     def sizeHint(self, option: QStyleOptionViewItem, index: QModelIndex | QPersistentModelIndex) -> QSize:
         options: QStyleOptionViewItem = QStyleOptionViewItem(option)
         self.initStyleOption(options, index)
-        doc: QTextDocument = QTextDocument(self)
-        doc.setHtml(options.text)
-        doc.setTextWidth(options.rect.width())
-        size: QSize = QSize(
-            round(doc.idealWidth()),
-            round(QTextDocument().size().height()),
-        )
-        doc.deleteLater()
-        return size
+        with the(self._doc) as doc:
+            doc.clear()
+            doc.setHtml(options.text)
+            doc.setTextWidth(options.rect.width())
+            return QSize(
+                round(doc.idealWidth()),
+                round(QTextDocument().size().height()),
+            )
